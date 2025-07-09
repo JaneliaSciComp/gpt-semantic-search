68895a4d0c9d6c1731b126da56976e4276946dae
Arjun Bakhale <arjun.bakhale@gmail.com>
2025-07-09 10:51:24 -0400

    resolve weaviate URL

--- 1_🔍_Search.py
+++ 1_🔍_Search.py
@@ -236,7 +236,7 @@ def get_response(_query_engine, _slack_client, query):
     return msg
 
 parser = argparse.ArgumentParser(description='Web service for semantic search using Weaviate and OpenAI')
-parser.add_argument('-w', '--weaviate-url', type=str, default="http://localhost:8777", help='Weaviate database URL')
+parser.add_argument('-w', '--weaviate-url', type=str, default="http://localhost:8080", help='Weaviate database URL')
 args = parser.parse_args()
 
 weaviate_client = get_weaviate_client(args.weaviate_url)
